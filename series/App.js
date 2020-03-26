64ee5da16e267077a3ccff36ba029868d4fcde8f
filipesluiz <filipesluiz@gmail.com>
2020-03-26 14:35:06 -0300

    Add dependencys react-navigation

--- series/App.js
+++ series/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-export default function App() {
+import LoginScreen from './src/pages/LoginScreen.js';
+
+const Stack = createStackNavigator();
+
+function App () {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="login" component={LoginScreen}/>
+        </Stack.Navigator>
+    </NavigationContainer>
   );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
