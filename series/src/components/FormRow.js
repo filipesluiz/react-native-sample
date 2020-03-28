a9bd491028eb5161de29072c97cd47812acba166
filipesluiz <filipesluiz@gmail.com>
2020-03-27 21:23:14 -0300

    Start page login

---
+++ series/src/components/FormRow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const FormRow = props => {
+    const {children} = props;
+    return(
+        <View style={styles.container}>
+            {children}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 5,
+        backgroundColor: '#fff4f2',
+        margin:5,
+        elevation: 1
+    }
+});
+
+export default FormRow;
--- series/src/pages/LoginScreen.js
+++ series/src/pages/LoginScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {View, TextInput} from 'react-native';
+import FormRow from '../components/FormRow';
 
 export default class LoginScreen extends React.Component {
     render(){
         return(
             <View>
-                <TextInput>Login</TextInput>
+                <FormRow>
+                    <TextInput placeholder="Login ou E-mail!" autoCompleteType="email" autoFocus={true}/>
+                </FormRow>    
+                <FormRow>
+                    <TextInput placeholder="Senha" secureTextEntry/>
+                </FormRow> 
             </View>
         );
     }
