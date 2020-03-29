5a88ca6aa658f95597d213e36acf2cd3d34b6e60
filipesluiz <filipesluiz@gmail.com>
2020-03-29 15:02:24 -0300

    add config and install firebase

--- series/App.js
+++ series/App.js
@@ -2,10 +2,28 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import LoginScreen from './src/pages/LoginScreen.js';
 
 const Stack = createStackNavigator();
+
+function configFirebase(){
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC07jlc08zjqGMfqWFSWjZY8PP2S4FYJ9c",
+    authDomain: "series-sample.firebaseapp.com",
+    databaseURL: "https://series-sample.firebaseio.com",
+    projectId: "series-sample",
+    storageBucket: "series-sample.appspot.com",
+    messagingSenderId: "908882369785",
+    appId: "1:908882369785:web:ae8ce9c3f899e1bac39628",
+    measurementId: "G-ZEJ756N5NG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+}
 
 function App () {
 
--- series/src/pages/LoginScreen.js
+++ series/src/pages/LoginScreen.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import {View, TextInput} from 'react-native';
+import {View, TextInput, Button} from 'react-native';
 import FormRow from '../components/FormRow';
 
-export default class LoginScreen extends React.Component {
-    render(){
-        return(
-            <View>
-                <FormRow>
-                    <TextInput placeholder="Login ou E-mail!" autoCompleteType="email" autoFocus={true}/>
-                </FormRow>    
-                <FormRow>
-                    <TextInput placeholder="Senha" secureTextEntry/>
-                </FormRow> 
-            </View>
-        );
-    }
+function log(login, password){
+   //console.log(login);
 }
+
+export default function LoginScreen () {
+    const [login, onChangeLogin] = React.useState(); //Returns a statefull value (variable) and a function to update it. 
+    const [password, onChangePassWord] = React.useState();
+    return (
+        <View>
+            <FormRow>
+                <TextInput 
+                    placeholder="Login ou E-mail!" autoCompleteType="email" autoFocus={true} 
+                    value={login} onChangeText={text => onChangeLogin(text)}/>
+            </FormRow>    
+            <FormRow>
+                <TextInput placeholder="Senha" secureTextEntry value={password} onChangeText={text => onChangePassWord(text)}/>
+            </FormRow> 
+            <Button title="Entrar" onPress={log(login, password)}/>
+        </View>
+    );
+}
+
